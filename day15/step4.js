690b36defffd070727f33dc89332af2c7b0132db
qudfoqjf <c8571@naver.com>
2023-12-16 14:45:35 +0900

    12-16 14:45

--- day15/step4.js
+++ day15/step4.js
@@ -13,9 +13,9 @@ function Register(){
     dateList.push(date); contentList.push(content); costList.push(cost);
     console.log(dateList);console.log(contentList);console.log(costList);
     //[3.출력]
+    
     output();
     Sum();
-    rest();
 }
 
 //2. 출력함수
@@ -23,6 +23,7 @@ function output(){
     const write=document.querySelector('#write');
     let html=''
     for (let i=0;i<dateList.length;i++){
+        rest(costList[i])
         html+= `<tr>
         <td>${dateList[i]}</td>
         <td>${contentList[i]}</td>
@@ -46,13 +47,42 @@ function Delete(delIndex) {
 function Sum(){
     const sum =document.querySelector('#sum');
     let total = 0;   
-    for(i=0;i<costList.length;i++){total+=Number(costList[i])}   
-    sum.innerHTML =Number(total);
+    for(i=0;i<costList.length;i++){total+=Number(costList[i])};
+    let a=total*1;
+    sum.innerHTML =rest(a);
 }
 
 //5. 쉼표함수
-function rest(){
-    let leng= ('cost').length; console.log(leng);
-    let slice = 'cost'.split ; console.log(slice)
-    if(leng>3){}
+function rest(tal){   
+    let to= String(tal); console.log(to);
+    let splt = to.split(''); console.log(splt);
+    let tt =''
+    if(splt.length%3==0){
+        for(i=1;i<=splt.length;i++){
+            if(i%3==0&& i!=splt.length){tt+=splt[i-1]+','}
+            else {tt+=splt[i-1]}
+            }
+    }
+    else if(splt.length%3==1){
+        for(i=1;i<=splt.length;i++){
+            if(i%3==1&& i!=splt.length){tt+=splt[i-1]+','}
+            else {tt+=splt[i-1]}
+        }
+    }
+    else if(splt.length%3==2){
+        for(i=1;i<=splt.length;i++){
+            if(i%3==2&& i!=splt.length){tt+=splt[i-1]+','}
+            else {tt+=splt[i-1]}
+        }
+    }
+    console.log (tt)
+    return tt
 }
+
+
+/* for(i=0;i<=splt.length-1;i++){
+        if(i%3==0){tt+=splt[i-1]+','}
+        else{tt+=splt[i-1]} ;
+        console.log(tt);
+    };
+    return tt; */
